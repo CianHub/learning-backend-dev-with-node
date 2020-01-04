00f1de064791b4db2681c366353a857441b6e8e8
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-04 16:28:17 +0000

    Add middleware, send response

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Middleware 1');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Middleware 2');
+  res.send('Hello World');
+});
+
 const server = http.createServer(app);
 server.listen(3000);
