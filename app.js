f78ecb88251eb27fd47635277a7afd4555d0f59c
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-04 16:39:06 +0000

    Add routes

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/egg', (req, res, next) => {
+  console.log('Middleware 3');
+  res.send('<h1>I am an egg</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('Middleware 1');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log('Middleware 2');
-  res.send('Hello World');
+  res.send('<h1>Hello World</h1>');
 });
 
 app.listen(3000);
