40698a4e5e8151578e985cf7a3888afd990fd1bc
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-05 19:30:58 +0000

    Replace shop.html with a pug template

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
