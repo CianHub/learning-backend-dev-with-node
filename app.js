b7701f6d4c831e2862b3cb278a2dfb13c9f36cbc
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-06 20:41:57 +0000

    convert app to use EJS templates

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const expressHandleBars = require('express-handlebars');
+//const expressHandleBars = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('handlebars', expressHandleBars());
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', expressHandleBars());
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
