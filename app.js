5f21dc3dd49442bc2741ba2002faa057f74e3e1d
Cian O'Gralaigh <ihatecian@gmail.com>
2020-01-08 22:59:18 +0000

    Add functionality for storing as JSON data in a file

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const pathUtil = require('../util/path');
 const products = [];
 
 module.exports = class Product {
@@ -5,7 +8,19 @@ module.exports = class Product {
     this.title = title;
   }
 
-  save = () => products.push(this);
+  save() {
+    const p = path.join(pathUtil, 'data', 'products');
+    fs.readFile(p, (err, data) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(data);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
   static fetchAll() {
     return products;
