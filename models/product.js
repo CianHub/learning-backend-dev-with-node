57653e9cb48a0328d88b1e7e96b949475ad0f7cd
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-18 11:30:52 +0100

    Create product schema

--- models/product.js
+++ models/product.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageURL: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
 /*const ObjectId = require('mongodb').ObjectId;
 const getDB = require('../util/database').getDB;
 
