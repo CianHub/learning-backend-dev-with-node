9bd763884ceedca713c96a14c544957436d4386b
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-18 13:03:59 +0100

    Add ref to mongoose schemas

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ const productSchema = new Schema({
   price: { type: Number, required: true },
   imageURL: { type: String, required: true },
   description: { type: String, required: true },
-  userId: { type: Schema.Types.ObjectId, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('Product', productSchema);
